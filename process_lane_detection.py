2a5d9d425d478b41e49d576c9cdcf9df3ee3f822
ssoonan <ssoonan0770@gmail.com>
2024-11-26 15:49:47 +0900

    process lane detection init

--- process_lane_detection.py
+++ process_lane_detection.py
@@ -1,79 +1,96 @@
+from fastdds import DomainParticipantFactory, DomainParticipantQos, TopicQos, PublisherQos, DataWriterQos, TypeSupport, RELIABLE_RELIABILITY_QOS
+
 from LaneDetection import LaneDetectionResult, LaneDetectionResultPubSubType
-from build.fastdds_python.src.swig.fastdds import DomainParticipantFactory
 from lib.lane_detector import enhanced_lane_detection
 from video_subscriber import read_frame, setup_fastdds_for_subscriber
-from video_publisher import setup_fastdds_for_publisher
+
 
 import cv2
 
 
-def publish_lane_detection_results(left_line, right_line):
-    lane_participant, datawriter = setup_fastdds_for_publisher("LaneDetection")
+def setup_fastdds_for_lane_detection():
+    """Fast DDS 초기화 및 주요 객체 생성"""
+    participant_qos = DomainParticipantQos()
+    participant = DomainParticipantFactory.get_instance().create_participant(0,
+                                                                             participant_qos)
 
     # Register the type
     lane_detection_type = LaneDetectionResultPubSubType()
-    participant.register_type(lane_detection_type)
-
-    # Create the Publisher
-    publisher = participant.create_publisher()
+    lane_detection_type.set_name("LaneDetection")
+    lane_detection_type_support = TypeSupport(
+        lane_detection_type)
+    participant.register_type(lane_detection_type_support)
 
     # Create the Topic
+    topic_qos = TopicQos()
     topic = participant.create_topic(
-        "LaneDetectionTopic", lane_detection_type.get_type_name())
+        "LaneDetectionTopic", lane_detection_type.get_name(), topic_qos)
 
-    # Create the DataWriter
-    writer_qos = DataWriterQos()
-    writer = publisher.create_datawriter(topic, writer_qos)
+    # Create the Publisher
+    publisher_qos = PublisherQos()
+    publisher = participant.create_publisher(publisher_qos)
+
+    datawriter_qos = DataWriterQos()
+    datawriter = publisher.create_datawriter(topic, datawriter_qos)
+
+    return participant, datawriter
+
+
+def publish_lane_detection_results(left_line, right_line):
+    lane_participant, datawriter = setup_fastdds_for_lane_detection()
 
     # Create the data instance
     lane_detection_result = LaneDetectionResult()
-
+    left_line_var = lane_detection_result.left_lane()
+    right_line_var = lane_detection_result.right_lane()
     # Fill in the left lane line data
     if left_line is not None:
-        lane_detection_result.left_lane.x1 = left_line[0][0]
-        lane_detection_result.left_lane.y1 = left_line[0][1]
-        lane_detection_result.left_lane.x2 = left_line[1][0]
-        lane_detection_result.left_lane.y2 = left_line[1][1]
+        left_line_var.x1(left_line[0][0])
+        left_line_var.y1(left_line[0][1])
+        left_line_var.x2(left_line[1][0])
+        left_line_var.y2(left_line[1][1])
     else:
-        lane_detection_result.left_lane.x1 = 0
-        lane_detection_result.left_lane.y1 = 0
-        lane_detection_result.left_lane.x2 = 0
-        lane_detection_result.left_lane.y2 = 0
+        left_line_var.x1(0)
+        left_line_var.y1(0)
+        left_line_var.x2(0)
+        left_line_var.y2(0)
 
     # Fill in the right lane line data
     if right_line is not None:
-        lane_detection_result.right_lane.x1 = right_line[0][0]
-        lane_detection_result.right_lane.y1 = right_line[0][1]
-        lane_detection_result.right_lane.x2 = right_line[1][0]
-        lane_detection_result.right_lane.y2 = right_line[1][1]
+        right_line_var.x1(right_line[0][0])
+        right_line_var.y1(right_line[0][1])
+        right_line_var.x2(right_line[1][0])
+        right_line_var.y2(right_line[1][1])
     else:
-        lane_detection_result.right_lane.x1 = 0
-        lane_detection_result.right_lane.y1 = 0
-        lane_detection_result.right_lane.x2 = 0
-        lane_detection_result.right_lane.y2 = 0
+        right_line_var.x1(0)
+        right_line_var.y1(0)
+        right_line_var.x2(0)
+        right_line_var.y2(0)
 
     # Publish the data
-    writer.write(lane_detection_result)
+    datawriter.write(lane_detection_result)
 
     # Clean up
-    participant.delete_contained_entities()
-    DomainParticipantFactory.get_instance().delete_participant(participant)
+    lane_participant.delete_contained_entities()
+    DomainParticipantFactory.get_instance().delete_participant(lane_participant)
 
 
-video_participant, datareader = setup_fastdds_for_subscriber("VideoData")
+def process_lane_detection_and_publish():
+    video_participant, datareader = setup_fastdds_for_subscriber()
+    try:
+        while True:
+            frame = read_frame(datareader)
+            if frame is None:
+                continue
+            left_line, right_line = enhanced_lane_detection(frame)
+            publish_lane_detection_results(left_line, right_line)
+
+    except KeyboardInterrupt:
+        print("Subscriber terminated.")
+    finally:
+        video_participant.delete_contained_entities()
+        DomainParticipantFactory.get_instance().delete_participant(video_participant)
 
 
-try:
-    while True:
-        frame = read_frame(datareader)
-        if frame is None:
-            continue
-        frame2 = enhanced_lane_detection(frame)
-
-
-except KeyboardInterrupt:
-    print("Subscriber terminated.")
-finally:
-    cv2.destroyAllWindows()
-    participant.delete_contained_entities()
-    DomainParticipantFactory.get_instance().delete_participant(participant)
+if __name__ == "__main__":
+    process_lane_detection_and_publish()
