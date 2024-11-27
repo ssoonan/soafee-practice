1858c9a984e5859a620ee69b580d7b51abcd162a
ssoonan <ssoonan0770@gmail.com>
2024-11-27 13:47:44 +0900

    dockerfile 에러 수정 및 compose, k8s yml

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 docker build -t ssoonan0770/soafee-practice:base . -f Dockerfile.base
 docker build -t ssoonan0770/soafee-practice:lane_detector . -f Dockerfile.lane_detector
-docker build -t ssoonan0770/soafee-practice:object_detector . -f Dockerfile.lane_detector
+docker build -t ssoonan0770/soafee-practice:object_detector . -f Dockerfile.object_detector
 docker build -t ssoonan0770/soafee-practice:flask . -f Dockerfile.flask
+
+
+docker push ssoonan0770/soafee-practice:lane_detector
+docker push ssoonan0770/soafee-practice:object_detector
+docker push ssoonan0770/soafee-practice:flask
