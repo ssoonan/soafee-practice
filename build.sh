c96714ac7942f7fc8190c2ef79308188357023fa
NamcheolLee <nclee@redwood.snu.ac.kr>
2024-12-09 16:32:21 +0900

    Adjusted permission of the build script

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ docker buildx inspect --bootstrap
 
 # 각 이미지 빌드
 # docker buildx build --platform linux/arm64 -t snurtos/soafee-practice:base . -f Dockerfile.base --push
+echo build start
 docker buildx build --platform linux/arm64 -t snurtos/soafee-practice:lane_detector . -f Dockerfile.lane_detector --push
 docker buildx build --platform linux/arm64 -t snurtos/soafee-practice:object_detector . -f Dockerfile.object_detector --push
 docker buildx build --platform linux/arm64 -t snurtos/soafee-practice:flask . -f Dockerfile.flask --push
