2a74fd27f214435833edc9d470f63e856a76af41
NamcheolLee <nclee@redwood.snu.ac.kr>
2024-12-09 16:30:46 +0900

    Adjusted permission of the build script

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
-docker build -t ssoonan0770/soafee-practice:base . -f Dockerfile.base
-docker build -t ssoonan0770/soafee-practice:lane_detector . -f Dockerfile.lane_detector
-docker build -t ssoonan0770/soafee-practice:object_detector . -f Dockerfile.object_detector
-docker build -t ssoonan0770/soafee-practice:flask . -f Dockerfile.flask
+# buildx 빌더 생성 및 사용 설치
+docker buildx create --name mybuilder --use
+docker buildx inspect --bootstrap
 
-
-docker push ssoonan0770/soafee-practice:lane_detector
-docker push ssoonan0770/soafee-practice:object_detector
-docker push ssoonan0770/soafee-practice:flask
+# 각 이미지 빌드
+# docker buildx build --platform linux/arm64 -t snurtos/soafee-practice:base . -f Dockerfile.base --push
+docker buildx build --platform linux/arm64 -t snurtos/soafee-practice:lane_detector . -f Dockerfile.lane_detector --push
+docker buildx build --platform linux/arm64 -t snurtos/soafee-practice:object_detector . -f Dockerfile.object_detector --push
+docker buildx build --platform linux/arm64 -t snurtos/soafee-practice:flask . -f Dockerfile.flask --push
